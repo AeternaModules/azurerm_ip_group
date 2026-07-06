fd15e83fa224940b1f2b0025a49beae9cb60bddd
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,32 @@ output "ip_groups" {
   description = "All ip_group resources"
   value       = azurerm_ip_group.ip_groups
 }
+output "ip_groups_cidrs" {
+  description = "List of cidrs values across all ip_groups"
+  value       = [for k, v in azurerm_ip_group.ip_groups : v.cidrs]
+}
+output "ip_groups_firewall_ids" {
+  description = "List of firewall_ids values across all ip_groups"
+  value       = [for k, v in azurerm_ip_group.ip_groups : v.firewall_ids]
+}
+output "ip_groups_firewall_policy_ids" {
+  description = "List of firewall_policy_ids values across all ip_groups"
+  value       = [for k, v in azurerm_ip_group.ip_groups : v.firewall_policy_ids]
+}
+output "ip_groups_location" {
+  description = "List of location values across all ip_groups"
+  value       = [for k, v in azurerm_ip_group.ip_groups : v.location]
+}
+output "ip_groups_name" {
+  description = "List of name values across all ip_groups"
+  value       = [for k, v in azurerm_ip_group.ip_groups : v.name]
+}
+output "ip_groups_resource_group_name" {
+  description = "List of resource_group_name values across all ip_groups"
+  value       = [for k, v in azurerm_ip_group.ip_groups : v.resource_group_name]
+}
+output "ip_groups_tags" {
+  description = "List of tags values across all ip_groups"
+  value       = [for k, v in azurerm_ip_group.ip_groups : v.tags]
+}
 
