e7ef81ce9034a4aaa0454dd3d23fa63ae0875e99
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "ip_groups" {
-  description = "All ip_group resources"
-  value       = azurerm_ip_group.ip_groups
-}
 output "ip_groups_cidrs" {
-  description = "List of cidrs values across all ip_groups"
-  value       = [for k, v in azurerm_ip_group.ip_groups : v.cidrs]
+  description = "Map of cidrs values across all ip_groups, keyed the same as var.ip_groups"
+  value       = { for k, v in azurerm_ip_group.ip_groups : k => v.cidrs }
 }
 output "ip_groups_firewall_ids" {
-  description = "List of firewall_ids values across all ip_groups"
-  value       = [for k, v in azurerm_ip_group.ip_groups : v.firewall_ids]
+  description = "Map of firewall_ids values across all ip_groups, keyed the same as var.ip_groups"
+  value       = { for k, v in azurerm_ip_group.ip_groups : k => v.firewall_ids }
 }
 output "ip_groups_firewall_policy_ids" {
-  description = "List of firewall_policy_ids values across all ip_groups"
-  value       = [for k, v in azurerm_ip_group.ip_groups : v.firewall_policy_ids]
+  description = "Map of firewall_policy_ids values across all ip_groups, keyed the same as var.ip_groups"
+  value       = { for k, v in azurerm_ip_group.ip_groups : k => v.firewall_policy_ids }
 }
 output "ip_groups_location" {
-  description = "List of location values across all ip_groups"
-  value       = [for k, v in azurerm_ip_group.ip_groups : v.location]
+  description = "Map of location values across all ip_groups, keyed the same as var.ip_groups"
+  value       = { for k, v in azurerm_ip_group.ip_groups : k => v.location }
 }
 output "ip_groups_name" {
-  description = "List of name values across all ip_groups"
-  value       = [for k, v in azurerm_ip_group.ip_groups : v.name]
+  description = "Map of name values across all ip_groups, keyed the same as var.ip_groups"
+  value       = { for k, v in azurerm_ip_group.ip_groups : k => v.name }
 }
 output "ip_groups_resource_group_name" {
-  description = "List of resource_group_name values across all ip_groups"
-  value       = [for k, v in azurerm_ip_group.ip_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all ip_groups, keyed the same as var.ip_groups"
+  value       = { for k, v in azurerm_ip_group.ip_groups : k => v.resource_group_name }
 }
 output "ip_groups_tags" {
-  description = "List of tags values across all ip_groups"
-  value       = [for k, v in azurerm_ip_group.ip_groups : v.tags]
+  description = "Map of tags values across all ip_groups, keyed the same as var.ip_groups"
+  value       = { for k, v in azurerm_ip_group.ip_groups : k => v.tags }
 }
 
